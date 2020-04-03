96cb0e7a28b5f8a842107dad382b1ce6e3f687dd
Mathias Thibaud <math.thibaud@gmail.com>
2020-04-03 12:15:08 -0700

    Init repo and created 4 routes

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const AboutPage = () => (
+  <>
+  <h1>About me</h1>
+  <p>
+    E iudicem eruditionem. Sint deserunt si iudicem. Appellat fore nulla singulis
+    velit, cillum pariatur se magna enim, aute et singulis, id vidisse sempiternum
+    qui fore appellat qui exercitation an amet ab arbitror ea quid a aliquip de
+    minim, de do fugiat mandaremus.Do aut aliqua litteris, cillum proident sed minim
+    quae. Cillum iudicem ab deserunt eu culpa se voluptate ut labore, ab nescius
+    comprehenderit, sunt senserit de anim tamen est consequat cillum singulis
+    mentitum, et id tamen fugiat nisi qui ut duis comprehenderit ita appellat
+    relinqueret ut mandaremus. Se excepteur est litteris, ingeniis multos admodum.
+    Aut illum quid si vidisse te ubi elit possumus, sed non voluptatibus, amet ita
+    qui sunt fabulas.
+  </p>
+  <p>
+    Labore proident an concursionibus nam ullamco eu noster pariatur, fore hic quo
+    quid commodo ne se esse si eram e fugiat si laborum a e qui veniam sunt dolor do
+    an laborum id possumus, expetendis veniam quem nostrud fugiat. Quamquam non
+    magna aliquip te fabulas qui dolor. Nostrud tempor appellat.An hic quem quid
+    anim. Se ab illum tamen nulla et duis mentitum an pariatur, occaecat tempor
+    ipsum fabulas duis, fugiat aut nam amet admodum. In minim quamquam cernantur o
+    de summis excepteur, an quorum non cillum te duis excepteur qui transferrem. Sed
+    cillum voluptatibus qui multos iudicem offendit hic veniam admodum ab
+    fidelissimae, nescius aute litteris cupidatat, offendit tempor singulis,
+    mentitum quem est nescius praesentibus, eiusmod tempor esse iis quis ex quo
+    magna quamquam voluptatibus.
+  </p>
+  </>
+);
+
+export default AboutPage;
